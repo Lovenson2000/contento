852b722c2cd26110ffc4fed784159e1a00068e3e
Lovenson <beaucicotlovenson@gmail.com>
2025-05-11 21:27:53 +0800

    feat: Added DateTime Picker for reminder

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -8,17 +8,21 @@ import {
   TextInput,
   Modal,
   ScrollView,
+  Keyboard,
+  KeyboardEvent,
+  Animated,
 } from "react-native";
 
 import Ionicons from "@expo/vector-icons/Ionicons";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
 import Entypo from "@expo/vector-icons/Entypo";
 import { contents } from "@/lib/constants/contents";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import SingleContentItem from "@/components/SingleContentItem";
 import ContentsFilters from "@/components/ContentFilters";
 import { socialMediaIcons } from "@/lib/constants/social-icons";
 import TagsFilter from "@/components/TagsFilter";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 export default function Index() {
   const [sourceModalVisible, setSourceModalVisible] = useState(false);
@@ -28,6 +32,7 @@ export default function Index() {
   const [selectedTags, setSelectedTags] = useState<string[]>([]);
   const [showFavorites, setShowFavorites] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
+  const [isAddContentModalVisible, setAddContentModalVisible] = useState(false);
 
   const allTags = Array.from(
     new Set(contents.flatMap((item) => item.tags ?? []))
@@ -36,6 +41,10 @@ export default function Index() {
   const handleSourceSelect = (source: string) => {
     setSelectedSource(source);
     setSourceModalVisible(false);
+  };
+
+  const handleAddContentModalClose = () => {
+    setAddContentModalVisible(false);
   };
 
   const toggleTag = (tag: string) => {
@@ -85,10 +94,14 @@ export default function Index() {
     <View className="bg-white flex-1 p-4">
       <View className="flex-row items-center justify-between mt-2 mb-6">
         <Text className="text-4xl font-semibold text-slate-800">My Saves</Text>
-        <Pressable>
+        <Pressable onPress={() => setAddContentModalVisible(true)}>
           <Entypo name="plus" size={40} color="#051542" />
         </Pressable>
       </View>
+      <AddNewContentModal
+        isVisible={isAddContentModalVisible}
+        onClose={handleAddContentModalClose}
+      />
       <View className="flex-row items-center border rounded-lg bg-gray-50 border-slate-100 px-2 py-0.5 mb-4">
         <Ionicons name="search" size={20} color="#64748b" className="mr-2" />
         <TextInput
@@ -161,6 +174,151 @@ export default function Index() {
           </View>
         </Modal>
       </SafeAreaView>
+    </View>
+  );
+}
+
+type AddNewContentModalProps = {
+  isVisible: boolean;
+  onClose: () => void;
+};
+export function AddNewContentModal({
+  isVisible,
+  onClose,
+}: AddNewContentModalProps) {
+  const [contentUrl, setContentUrl] = useState("");
+  const [tags, setTags] = useState("");
+  const [showDateTimePicker, setShowDateTimePicker] = useState(false);
+  const [reminderDate, setReminderDate] = useState<Date | null>(null);
+  const modalHeight = useRef(new Animated.Value(0.5)).current;
+
+  const onChangeDate = (event: any, selectedDate?: Date) => {
+    setShowDateTimePicker(false);
+    if (selectedDate) {
+      setReminderDate(selectedDate);
+    }
+  };
+
+  useEffect(() => {
+    const keyboardShow = Keyboard.addListener(
+      "keyboardDidShow",
+      _keyboardDidShow
+    );
+    const keyboardHide = Keyboard.addListener(
+      "keyboardDidHide",
+      _keyboardDidHide
+    );
+
+    return () => {
+      keyboardShow.remove();
+      keyboardHide.remove();
+    };
+  }, []);
+
+  const _keyboardDidShow = (e: KeyboardEvent) => {
+    Animated.timing(modalHeight, {
+      toValue: 0.85,
+      duration: 250,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const _keyboardDidHide = () => {
+    Animated.timing(modalHeight, {
+      toValue: 0.5,
+      duration: 250,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  return (
+    <View>
+      <Modal animationType="slide" transparent={true} visible={isVisible}>
+        <View className="flex-1 bg-black/50 justify-end">
+          <Animated.View
+            style={{
+              height: modalHeight.interpolate({
+                inputRange: [0, 1],
+                outputRange: ["0%", "100%"],
+              }),
+            }}
+            className="w-full bg-white p-4 rounded-t-2xl"
+          >
+            <View className="w-full h-1/2 bg-white p-4 rounded-t-2xl">
+              <View className="rounded-t-xl text-center relative">
+                <Pressable onPress={onClose} className="absolute top-0 right-0">
+                  <MaterialCommunityIcons
+                    name="close"
+                    color="#051542"
+                    size={24}
+                  />
+                </Pressable>
+                <Text className="text-center text-2xl my-8">
+                  Add New Content
+                </Text>
+              </View>
+              <View className="mt-6">
+                <Text className="text-[##051542] text-xl font-medium mb-1">
+                  URL
+                </Text>
+                <TextInput
+                  onChangeText={setContentUrl}
+                  value={contentUrl}
+                  placeholder="e.g https://www.contento.dev/"
+                  placeholderTextColor="#535c73"
+                  className="border border-slate-200 bg-white rounded-lg p-4 text-slate-900"
+                />
+              </View>
+
+              <View className="mt-4">
+                <Text className="text-[##051542] text-xl font-medium mb-1">
+                  Tags (optional)
+                </Text>
+                <TextInput
+                  onChangeText={setTags}
+                  value={tags}
+                  placeholder="e.g education, news, science"
+                  placeholderTextColor="#535c73"
+                  className="border border-slate-200 bg-white rounded-lg p-4 text-slate-900"
+                />
+              </View>
+
+              {/* Reminder */}
+              <View className="mt-4">
+                <Pressable
+                  onPress={() => setShowDateTimePicker(true)}
+                  className="py-2"
+                >
+                  <Text className="text-[#364aca] font-medium">
+                    {reminderDate
+                      ? `Reminder: ${reminderDate.toLocaleString()}`
+                      : "Add Reminder"}
+                  </Text>
+                </Pressable>
+                {showDateTimePicker && (
+                  <DateTimePicker
+                    value={reminderDate || new Date()}
+                    mode="datetime"
+                    display="default"
+                    onChange={onChangeDate}
+                  />
+                )}
+              </View>
+              <View className="flex-row gap-x-4 mt-6">
+                <Pressable
+                  onPress={onClose}
+                  className="flex-1 px-4 py-3 border border-gray-300 rounded-lg bg-gray-100 items-center"
+                >
+                  <Text className="text-gray-700 font-medium">Cancel</Text>
+                </Pressable>
+                <Pressable className="flex-1 px-4 py-3 bg-[#364aca] rounded-lg items-center">
+                  <Text className="text-white font-medium">Save Content</Text>
+                </Pressable>
+              </View>
+            </View>
+          </Animated.View>
+        </View>
+      </Modal>
     </View>
   );
 }

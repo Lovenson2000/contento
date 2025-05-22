9536e680639d4f69abc09f865346f6944a9b1887
Lovenson <beaucicotlovenson@gmail.com>
2025-05-22 20:41:09 +0800

    refactor: Refactor Header for title management

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,39 +1,32 @@
 import { View, Text, Pressable, SafeAreaView } from "react-native";
 import { useRouter, usePathname } from "expo-router";
-import { useEffect, useState } from "react";
 
 export default function Header() {
   const router = useRouter();
   const pathname = usePathname();
-  const [currentPath, setCurrentPath] = useState(pathname);
-  const isHomePage = currentPath === "/";
-  const isSavedContentPage = currentPath === "/saved-content";
 
   const titleMap: Record<string, string> = {
     "/": "Home",
     "/(tabs)": "Home",
     "/(tabs)/index": "Home",
-    "/(tabs)/saved-content": "Saved",
+    "/settings": "Settings",
     "/(home)/content-details": "Details",
   };
 
-  const [title, setTitle] = useState(titleMap[pathname] || "Content");
-
-  useEffect(() => {
-    setTitle(titleMap[pathname] || "Content");
-    setCurrentPath(pathname);
-  }, [pathname]);
+  const title = titleMap[pathname] || "Content";
+  const isHomePage = pathname === "/";
+  const isSettingsPage = pathname === "/settings";
 
   return (
     <SafeAreaView style={{ backgroundColor: "#ffffff" }}>
       <View
         className={`h-16 bg-white ${
-          isHomePage || isSavedContentPage
+          isHomePage || isSettingsPage
             ? "items-center"
             : "flex-row items-center justify-between"
         } px-4 border-b border-slate-200`}
       >
-        {isHomePage || isSavedContentPage ? (
+        {isHomePage || isSettingsPage ? (
           <Text className="text-2xl text-center font-medium center text-slate-900">
             {title}
           </Text>

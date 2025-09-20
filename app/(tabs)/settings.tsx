506ad3e0f9cb3c33bf2b9b109c17a4577adad6cd
Lovenson <beaucicotlovenson@gmail.com>
2025-09-20 21:16:27 +0800

    feat: Added Icons to Settings Options

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,15 +1,20 @@
-import { useAuth } from "@/context/AuthContext";
 import { supabase } from "@/lib/supabase";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { useRouter } from "expo-router";
 import React from "react";
-import { View, Text, Pressable, ScrollView, Image } from "react-native";
+import {
+  View,
+  Text,
+  Pressable,
+  ScrollView,
+  Image,
+  ImageSourcePropType,
+} from "react-native";
 
-const LogoImage = require("@/assets/images/full-logo.png");
+const DeleteAccountIcon = require("@/assets/images/lock.png");
+const LogOutIcon = require("@/assets/images/log-out.png");
+const ContactUsIcon = require("@/assets/images/envelope.png");
+const PrivacyAndPolicyIcon = require("@/assets/images/shield.png");
 
 export default function SettingsScreen() {
-  const user = useAuth()?.user;
-
   const signout = async () => {
     const { error } = await supabase.auth.signOut();
     if (error) {
@@ -21,20 +26,21 @@ export default function SettingsScreen() {
 
   return (
     <ScrollView className="flex-1 bg-white">
-      <View className="p-4">
-        <SectionHeader title="Account" />
-        <Pressable
+      <View className="p-4 w-full">
+        <SectionHeader title="Account Settings" />
+        <SettingsOption
+          iconSource={LogOutIcon}
+          label="Log out"
           onPress={signout}
-          className="flex-row gap-4 items-center py-4"
-        >
-          <MaterialCommunityIcons name="logout" size={24} color="#051542" />
-          <Text className="text-[#051542] text-xl">Log out</Text>
-        </Pressable>
-        <SettingsOption icon="❌" label="Delete Account" />
+        />
+        <SettingsOption iconSource={DeleteAccountIcon} label="Delete Account" />
 
         <SectionHeader title="Support" />
-        <SettingsOption icon="📞" label="Contact Us" />
-        <SettingsOption icon="🔒" label="Privacy and Policy" />
+        <SettingsOption iconSource={ContactUsIcon} label="Contact Us" />
+        <SettingsOption
+          iconSource={PrivacyAndPolicyIcon}
+          label="Privacy and Policy"
+        />
       </View>
     </ScrollView>
   );
@@ -43,25 +49,33 @@ export default function SettingsScreen() {
 export function SectionHeader({ title }: { title: string }) {
   return (
     <View className="mt-4 mb-2">
-      <Text className="text-slate-500 font-semibold text-sm">{title}</Text>
+      <Text className="text-slate-500 font-bold text-[1.4rem]">{title}</Text>
     </View>
   );
 }
 
 interface SettingsOptionProps {
-  icon: string;
+  iconSource: ImageSourcePropType;
   label: string;
   onPress?: () => void;
 }
 
-export function SettingsOption({ icon, label, onPress }: SettingsOptionProps) {
+export function SettingsOption({
+  iconSource,
+  label,
+  onPress,
+}: SettingsOptionProps) {
   return (
     <Pressable
       onPress={onPress}
       className="flex-row items-center py-4 border-b border-slate-200"
     >
-      <Text className="text-xl mr-3">{icon}</Text>
-      <Text className="text-base text-slate-800">{label}</Text>
+      <Image
+        source={iconSource}
+        style={{ width: 28, height: 28, marginRight: 12 }}
+        resizeMode="contain"
+      />
+      <Text className="text-[1.2rem] font-medium text-slate-800">{label}</Text>
     </Pressable>
   );
 }

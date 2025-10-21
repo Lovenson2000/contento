bdc774ee6c38bd6278ab102599ef41c021403a6d
Lovenson <beaucicotlovenson@gmail.com>
2025-10-21 13:39:02 +0800

    feat: Added correct prefix for environment variables

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,13 +1,13 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
-// const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseServiceRoleKey =
+  process.env.EXPO_PUBLIC_SUPABASE_SERVICE_ROLE_KEY;
 
 if (!supabaseUrl || !supabaseServiceRoleKey) {
   throw new Error(
-    "Supabase URL and Service Role Key are not found in env variables."
+    "Supabase URL and Service Role Key are not found in env variables"
   );
 }
 
